0b8b5eb1a532cf90363a01e95b68597ccd291192
yanivraveh <yanivraveh@gmail.com>
2025-01-12 13:11:07 +0200

    removes darkmode from globals.css, adds color to title in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,7 +81,7 @@ export default function Home() {
       {/* Benefits Section */}
       <div className="bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
-          <h2 className="text-3xl font-semibold mb-4">{pageData.benefits.title}</h2>
+          <h2 className="text-3xl font-semibold text-gray-800 mb-4">{pageData.benefits.title}</h2>
           <p className="text-xl text-gray-800 mb-12">{pageData.benefits.subtitle}</p>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
             {pageData.benefits.items.map((benefit, index) => (
